e7963e416f7d26abc74a98c20d3bbd9474abb2a6
Jiro Ricaro <ricarovirgiliojr@gmail.com>
2020-11-28 21:44:35 +0800

    module 126 changes
    built the project file structure
    added initial needed components
    added a few stylings

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
+
+const layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar, SideDrawer, Backdrop
+        </div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
