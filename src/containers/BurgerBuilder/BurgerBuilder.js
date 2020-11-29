cfe5202998a7ea154f0791e2d6c876063dc40fe7
Jiro Ricaro <ricarovirgiliojr@gmail.com>
2020-11-30 02:27:31 +0800

    added remove ingredient function
    added remove ingredient function to the BuildControl component, and added a safety measure wherein if a specific ingredient is 0, the user wouldn't be able to press "less" as this will make the ingredient of a particular type, negative.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,15 +39,43 @@ class BurgerBuilder extends Component {
          
     }
 
-    removeIngredientHandler = () => {
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
 
+        if( oldCount <= 0) {
+            return;
+        }
+
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render() {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls ingredientAdded={this.addIngredientHandler} />
+                <BuildControls 
+                ingredientAdded={this.addIngredientHandler} 
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}/>
             </Aux>
         );
     }
