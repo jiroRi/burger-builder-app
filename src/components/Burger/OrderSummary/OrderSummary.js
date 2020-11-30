b4ecdeb806826286c3336e71e4ae41b28feb8037
Jiro Ricaro <ricarovirgiliojr@gmail.com>
2020-12-01 02:04:33 +0800

    added the total price to the OrderSummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -12,12 +12,13 @@ const orderSummary = (props) => {
             </li>
         );
     });
-    
+
     return (
         <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>{ingredientSummary}</ul>
+            <p><b>Total Price:</b> PHP {props.price}</p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.orderCanceled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.orderContinued}>CONTINUE</Button>
