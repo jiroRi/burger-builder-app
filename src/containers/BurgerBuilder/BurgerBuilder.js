f4f11a57c270aecd53afb5a4fabe64f76ced0eaa
Jiro Ricaro <ricarovirgiliojr@gmail.com>
2020-11-30 23:20:19 +0800

    Added Modal and Order Summary
    Added a modal(can't show/hide for now) and within it an Order Summary, for the dynamic ingredient count

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
 
 const INGREDIENT_PRICES = {
     salad: 7,
@@ -84,6 +87,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                 ingredientAdded={this.addIngredientHandler} 
