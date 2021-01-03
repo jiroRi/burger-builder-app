7750d29b66628ca9d5a002facc32f06997e8fcbf
Jiro Ricaro <ricarovirgiliojr@gmail.com>
2021-01-04 02:50:10 +0800

    added CheckoutSummary component
    added CheckoutSummary component
    added BrowserRouter to this app
    added Checkout container

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            patty: 1,
+            cheese: 1,
+            bacon: 1,
+        }
+    }
+    
+    render () {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}/>
+            </div>
+        );
+    }
+}
+
+export default Checkout;
